aabff84ddae6bb73ee65125525a5637bc2d2ea6a
Maina Zaquir <mzaquir58@gmail.com>
2024-03-03 16:44:31 +0300

    Completed Login form

--- event-planner/src/App.js
+++ event-planner/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/LoginForm';
+import Registration from './components/RegistrationForm';
+import Dashboard from './components/DashboardForm';
+import EventManagement from './components/EventManagementForm';
+import TaskManagement from './components/TaskManagementForm';
+import ResourceManagement from './components/ResourceManagement';
+import Collaboration from './components/CollaborationForm';
+import BudgetManagement from './components/BudgetManagementForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" component={Registration} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/events" component={EventManagement} />
+          <Route path="/tasks" component={TaskManagement} />
+          <Route path="/resources" component={ResourceManagement} />
+          <Route path="/collaboration" component={Collaboration} />
+          <Route path="/budget" component={BudgetManagement} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
