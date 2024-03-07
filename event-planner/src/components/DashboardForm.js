d77153db18dc13c243ba6adefa539e99361d0245
hazel <muthoninjoki2003@gamil.com>
2024-03-07 13:17:49 +0300

    made changes

--- event-planner/src/components/DashboardForm.js
+++ event-planner/src/components/DashboardForm.js
@@ -1,11 +1,52 @@
-import React from 'react'
+document.getElementById('dashboardForm').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-function DashboardForm() {
-  return (
-    <div>
-      DashboardForm
-    </div>
-  )
-}
+  const eventName = document.getElementById('eventName').value;
+  const eventDate = document.getElementById('eventDate').value;
+  const eventLocation = document.getElementById('eventLocation').value;
 
-export default DashboardForm
+  const eventNameError = document.getElementById('eventNameError');
+  const eventDateError = document.getElementById('eventDateError');
+  const eventLocationError = document.getElementById('eventLocationError');
+
+  let isValid = true;
+
+  if (eventName === '') {
+    eventNameError.textContent = 'Event name is required.';
+    isValid = false;
+  } else {
+    eventNameError.textContent = '';
+  }
+
+  if (eventDate === '') {
+    eventDateError.textContent = 'Event date is required.';
+    isValid = false;
+  } else {
+    eventDateError.textContent = '';
+  }
+
+  if (eventLocation === '') {
+    eventLocationError.textContent = 'Event location is required.';
+    isValid = false;
+  } else {
+    eventLocationError.textContent = '';
+  }
+
+  if (isValid) {
+    const output = document.getElementById('eventsTable');
+    const row = output.insertRow(-1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+
+    cell1.textContent = eventName;
+    cell2.textContent = eventDate;
+    cell3.textContent = eventLocation;
+  }
+});
+
+document.getElementById('clearForm').addEventListener('click', function() {
+  document.getElementById('eventName').value = '';
+  document.getElementById('eventDate').value = '';
+  document.getElementById('eventLocation').value = '';
+});
--- event-planner/src/components/NavBar.js
+++ event-planner/src/components/NavBar.js
@@ -1,38 +1,85 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css'; 
+import { FaBars } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSidebar } from '../features/sidebar/sidebarSlice';
+import { selectUser } from '../features/user/userSlice';
 
-function Navbar() {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="navbar-logo">
-                EVENT PLANNER
-            </Link>
-            <ul className="navbar-links">
-                <li>
-                    <Link to="/register" className="navbar-link">Register</Link>
-                </li>
-                <li>
-                    <Link to="/dashboard" className="navbar-link">Dashboard</Link>
-                </li>
-                <li>
-                    <Link to="/events" className="navbar-link">Events</Link>
-                </li>
-                <li>
-                    <Link to="/tasks" className="navbar-link">Tasks</Link>
-                </li>
-                <li>
-                    <Link to="/resources" className="navbar-link">Resources</Link>
-                </li>
-                <li>
-                    <Link to="/collaboration" className="navbar-link">Collaboration</Link>
-                </li>
-                <li>
-                    <Link to="/budget" className="navbar-link">Budget</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+function NavBar() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const handleToggleSidebar = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleToggleSidebar}
+        >
+          <FaBars />
+        </button>
+        <Link to="/" className="navbar-brand">
+          Event Time
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/events" className="nav-link">
+                Events
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-link">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-nav ml-auto">
+          {user ? (
+            <>
+              <span className="nav-item nav-link">{user.name}</span>
+              <button
+                className="nav-item nav-link btn btn-outline-danger"
+                onClick={() => console.log('Logout')}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="nav-item nav-link">
+                Login
+              </Link>
+              <Link to="/register" className="nav-item nav-link">
+                Register
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
