d40c8fa9d1db823413abd25aa59fb4e2528298bd
Maina Zaquir <mzaquir58@gmail.com>
2024-03-14 13:07:55 +0300

    modified the landing page

--- event-planner/src/index.js
+++ event-planner/src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
