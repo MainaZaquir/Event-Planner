532af2cb7174160cb1a60c09ca33d037bcc24e34
+ <+>
2024-03-18 16:27:17 +0300

    debugging

--- event-planner/src/App.js
+++ event-planner/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as  Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
 import DashboardForm from './components/DashboardForm';
